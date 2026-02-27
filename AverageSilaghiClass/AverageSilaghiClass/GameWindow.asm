c4e014b80ab52f5dd03bbd470edf68b3036ea9fa
PepsiMann620 <pepsimann620@gmail.com>
2026-02-27 13:06:49 -0500

    Added sample layout
    Made a simple "split screen" layout.

--- AverageSilaghiClass/AverageSilaghiClass/GameWindow.asm
+++ AverageSilaghiClass/AverageSilaghiClass/GameWindow.asm
@@ -4,7 +4,7 @@ INCLUDE Irvine32.inc
 
 .data
 hBorder BYTE "+-------------------------------------++-------------------------------------+", 0
-vBorder BYTE "|", 0
+vBorder BYTE "|                                     ||                                     |", 0
 
 .code
 PUBLIC UpdateScreen
@@ -21,11 +21,34 @@ DrawBase PROC
 	call Clrscr
 
 	; Adding borders
-	mov eax, lightGray + (gray * 16)
+	mov eax, gray + (black * 16)
 	call SetTextColor
 
 	; Adding top border
 	mov dh, 0
+	mov dl, 0
+	call Gotoxy
+	mov edx, OFFSET hBorder
+	call WriteString
+
+	; Adding vertical borders
+	mov cl, 0
+	vBorder_loop_start:
+		mov dh, cl
+		add dh, 1
+
+		mov dl, 0
+		call Gotoxy
+		mov edx, OFFSET vBorder
+		call WriteString
+
+		inc cl
+		.if cl < 22
+			jmp vBorder_loop_start
+		.endif
+
+	; Adding bottom border
+	mov dh, 23
 	mov dl, 0
 	call Gotoxy
 	mov edx, OFFSET hBorder
