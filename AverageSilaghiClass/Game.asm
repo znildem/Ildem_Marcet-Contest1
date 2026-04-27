ba038317e03f7fef1e4e67332e2911711f773cbd
znildem <nevaildem@gmail.com>
2026-04-27 16:10:10 -0400

    Add animated countdown with fixed rendering bug

--- AverageSilaghiClass/Game.asm
+++ AverageSilaghiClass/Game.asm
@@ -24,6 +24,8 @@ DinoTick PROTO
 DinoIsDone PROTO
 DinoWasSuccess PROTO
 ApplyDinoPenalty PROTO
+DrawDinoCountdown PROTO
+DrawDinoBase PROTO
 
 LoadQuiz PROTO
 LoadLab PROTO
@@ -52,8 +54,189 @@ scoreSuffix50   BYTE "/50", 0
 scoreTotalLabel BYTE "Total Score: ", 0
 scoreSuffix100  BYTE "/100", 0
 
+; Countdown
+countThree1 BYTE 219,219,219,219,219,0
+countThree2 BYTE "    ",219,0
+countThree3 BYTE 219,219,219,219,219,0
+countThree4 BYTE "    ",219,0
+countThree5 BYTE 219,219,219,219,219,0
+
+countTwo1 BYTE 219,219,219,219,219,0
+countTwo2 BYTE "    ",219,0
+countTwo3 BYTE 219,219,219,219,219,0
+countTwo4 BYTE 219,"    ",0
+countTwo5 BYTE 219,219,219,219,219,0
+
+countOne1 BYTE "  ",219,"  ",0
+countOne2 BYTE " ",219,219,"  ",0
+countOne3 BYTE "  ",219,"  ",0
+countOne4 BYTE "  ",219,"  ",0
+countOne5 BYTE 219,219,219,219,219,0
+
+countGo1 BYTE 219,219,219,"  ",219,219,219, " ", 219,0
+countGo2 BYTE 219,"    ",219," ",219," ", 219, 0
+countGo3 BYTE 219," ",219,219," ",219," ",219," ", 219, 0
+countGo4 BYTE 219,"  ",219," ",219," ",219, " ",0
+countGo5 BYTE 219,219,219,"  ",219,219,219," ", 219, 0
+
+countStartRow BYTE 0
+countStartCol BYTE 0
+countLine1 DWORD 0
+countLine2 DWORD 0
+countLine3 DWORD 0
+countLine4 DWORD 0
+countLine5 DWORD 0
+
 .code
 PUBLIC Game
+
+DrawBigCountdown PROC
+	; ESI = line 1
+	; EDI = line 2
+	; EBX = line 3
+	; ECX = line 4
+	; countLine5 already holds line 5
+	; DH = start row
+	; DL = start column
+
+	mov countStartRow, dh
+	mov countStartCol, dl
+	mov countLine1, esi
+	mov countLine2, edi
+	mov countLine3, ebx
+	mov countLine4, ecx
+
+	mov dh, countStartRow
+	mov dl, countStartCol
+	call BufGotoxy
+	mov edx, countLine1
+	call BufWriteString
+
+	mov dh, countStartRow
+	add dh, 1
+	mov dl, countStartCol
+	call BufGotoxy
+	mov edx, countLine2
+	call BufWriteString
+
+	mov dh, countStartRow
+	add dh, 2
+	mov dl, countStartCol
+	call BufGotoxy
+	mov edx, countLine3
+	call BufWriteString
+
+	mov dh, countStartRow
+	add dh, 3
+	mov dl, countStartCol
+	call BufGotoxy
+	mov edx, countLine4
+	call BufWriteString
+
+	mov dh, countStartRow
+	add dh, 4
+	mov dl, countStartCol
+	call BufGotoxy
+	mov edx, countLine5
+	call BufWriteString
+
+	ret
+DrawBigCountdown ENDP
+
+DrawDinoCountdown PROC
+	push eax
+	push edx
+
+	; 3
+	call BufClearScreen
+	call DrawDinoBase
+	mov eax, lightRed + (black * 16)
+	call BufSetTextColor
+    mov esi, OFFSET countThree1
+	mov edi, OFFSET countThree2
+	mov ebx, OFFSET countThree3
+	mov ecx, OFFSET countThree4
+	mov countLine5, OFFSET countThree5
+	mov dh, 8
+	mov dl, 37
+	call DrawBigCountdown
+    call FlushScreenBuffer
+    mov eax, 500
+    call Delay
+
+	call BufClearScreen
+    call DrawDinoBase
+    call FlushScreenBuffer
+    mov eax, 200
+    call Delay
+
+	; 2
+	call BufClearScreen
+    call DrawDinoBase
+    mov eax, brown + (black * 16)
+    call BufSetTextColor
+    mov esi, OFFSET countTwo1
+	mov edi, OFFSET countTwo2
+	mov ebx, OFFSET countTwo3
+	mov ecx, OFFSET countTwo4
+	mov countLine5, OFFSET countTwo5
+	mov dh, 8
+	mov dl, 37
+	call DrawBigCountdown
+    call FlushScreenBuffer
+    mov eax, 500
+    call Delay
+
+    call BufClearScreen
+    call DrawDinoBase
+    call FlushScreenBuffer
+    mov eax, 200
+    call Delay
+
+	; 1
+	call BufClearScreen
+    call DrawDinoBase
+	mov eax, yellow + (black * 16)
+    call BufSetTextColor
+    mov esi, OFFSET countOne1
+	mov edi, OFFSET countOne2
+	mov ebx, OFFSET countOne3
+	mov ecx, OFFSET countOne4
+	mov countLine5, OFFSET countOne5
+	mov dh, 8
+	mov dl, 37
+	call DrawBigCountdown
+    call FlushScreenBuffer
+    mov eax, 500
+    call Delay
+
+    call BufClearScreen
+    call DrawDinoBase
+    call FlushScreenBuffer
+    mov eax, 200
+    call Delay
+
+	; GO
+	call BufClearScreen
+    call DrawDinoBase
+    mov eax, lightGreen + (black * 16)
+    call BufSetTextColor
+	mov esi, OFFSET countGo1
+	mov edi, OFFSET countGo2
+	mov ebx, OFFSET countGo3
+	mov ecx, OFFSET countGo4
+	mov countLine5, OFFSET countGo5
+	mov dh, 8
+	mov dl, 34
+	call DrawBigCountdown
+    call FlushScreenBuffer
+    mov eax, 600
+    call Delay
+
+    pop edx
+    pop eax
+    ret
+DrawDinoCountdown ENDP
 
 DrawEndScreen PROC
     push eax
@@ -139,6 +322,7 @@ press_enter_loop_start:
 
     ; STATE 1: getting quiz (dino)
     mov currState, 1
+	call DrawDinoCountdown
     call DinoInit
 
 getting_quiz_loop_start:
@@ -195,6 +379,7 @@ quiz_loop_start:
 
     ; STATE 3: turning in quiz (dino)
     mov currState, 3
+	call DrawDinoCountdown
     call DinoInit
 
 turning_in_quiz_loop_start:
@@ -225,6 +410,7 @@ state3_done:
     call SwitchTimers
     movzx eax, chosenDifficulty
     call LoadLab
+	call DrawDinoCountdown
     call DinoInit
 
 getting_lab_loop_start:
@@ -281,6 +467,7 @@ lab_loop_start:
 
     ; STATE 6: turning in lab (dino)
     mov currState, 6
+	call DrawDinoCountdown
     call DinoInit
 
 turning_in_lab_loop_start:
